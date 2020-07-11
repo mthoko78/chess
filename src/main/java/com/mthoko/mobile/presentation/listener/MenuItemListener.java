bb46d7d4f1bd23ee6d487f5a2310ba5ece42deeb
Mthokozisi Mhlanga <mthokozisi@psybergate.co.za>
2020-07-11 02:27:28 +0200

    deleted package

--- src/main/java/com/mthoko/mobile/presentation/listener/MenuItemListener.java
+++
@@ -1,4 +0,0 @@
-package com.mthoko.mobile.presentation.listener;
-
-public interface MenuItemListener {
-}
--- src/main/java/com/mthoko/mobile/presentation/listener/MenuItemSelector.java
+++
@@ -1,7 +0,0 @@
-package com.mthoko.mobile.presentation.listener;
-
-import android.view.MenuItem;
-
-public interface MenuItemSelector {
-    public void onItemSelect(MenuItem item, int position);
-}
