84ca1dced1d00b2305afda0eaa2517e7cc88f6ab
Mthokozisi Mhlanga <mthokozisi@psybergate.co.za>
2020-08-02 14:14:08 +0200

    added mapping for saving location stamp

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -39,6 +39,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -257,7 +258,7 @@ public class Main {
 		}
 	}
 
-	@RequestMapping("/save-location-stamp")
+	@PostMapping("/save-location-stamp")
 	@ResponseBody
 	public Object saveLocationStamp(@RequestBody LocationStamp locationStamp) {
 		try {
