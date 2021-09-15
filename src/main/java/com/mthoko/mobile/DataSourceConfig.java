4f942fb33385a74ee1ce8a1c690183d1bc0ca47b
mthokozisi mhlanga <mthoko78@gmail.com>
2021-09-15 12:55:52 +0200

    modified datasource configuration

--- src/main/java/com/mthoko/mobile/DataSourceConfig.java
+++ src/main/java/com/mthoko/mobile/DataSourceConfig.java
@@ -9,19 +9,34 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
-    
+
+    public static final String PRODUCTION = "production";
+
     @Bean
     public DataSource getDataSource(
-    		@Value("${datasource.driver-class-name}") String driverClassName, 
-    		@Value("${datasource.url}") String url, 
-    		@Value("${datasource.username}") String username, 
-    		@Value("${datasource.password}") String password
-    		) {
+            @Value("${datasource.driver-class-name}") String driverClassName,
+            @Value("${datasource.url}") String url,
+            @Value("${datasource.username}") String username,
+            @Value("${datasource.password}") String password,
+            @Value("${dev.datasource.driver-class-name}") String devDriverClassName,
+            @Value("${dev.datasource.url}") String devUrl,
+            @Value("${dev.datasource.username}") String devUsername,
+            @Value("${dev.datasource.password}") String devPassword,
+            @Value("${spring.profiles.active}") String activeProfile
+    ) {
+        if (!PRODUCTION.equals(activeProfile)) {
+            return getDataSourceFromProperties(devDriverClassName, devUrl, devUsername, devPassword);
+        }
+        return getDataSourceFromProperties(driverClassName, url, username, password);
+
+    }
+
+    private DataSource getDataSourceFromProperties(String driverClassName, String url, String username, String password) {
         DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
-		dataSourceBuilder.driverClassName(driverClassName);
-		dataSourceBuilder.url(url);
-		dataSourceBuilder.username(username);
-		dataSourceBuilder.password(password);
+        dataSourceBuilder.driverClassName(driverClassName);
+        dataSourceBuilder.url(url);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
         return dataSourceBuilder.build();
     }
 }
