66187b7d7482ff600b3973dbbdea121a0abe0b72
Mthokozisi Mhlanga <mthokozisi.mhlanga@entelect.co.za>
2021-11-04 18:22:30 +0200

    corrected qccount retrieval query

--- src/main/java/com/mthoko/learners/domain/account/AccountRepo.java
+++ src/main/java/com/mthoko/learners/domain/account/AccountRepo.java
@@ -9,15 +9,15 @@ import java.util.Optional;
 @Repository
 public interface AccountRepo extends JpaRepository<Account, Long> {
 
-    @Query("select a from Account a join Member m on a.member.id = a.id where m.email = ?1")
+    @Query("select a from Account a join Member m on a.member.id = m.id where m.email = ?1")
     Optional<Account> findByEmail(String email);
 
-    @Query("select a from Account a join Member m on a.member.id = a.id where m.phone = ?1")
+    @Query("select a from Account a join Member m on a.member.id = m.id where m.phone = ?1")
     Optional<Account> findByPhone(String phone);
 
     @Query("select a from Account a join Device d on a.member.id = d.memberId where d.imei = ?1")
     Optional<Account> findByImei(String imei);
 
-    @Query("select a from Account a join Member m on a.member.id = a.id where m.id = ?1")
+    @Query("select a from Account a join Member m on a.member.id = m.id where m.id = ?1")
     Account findByMemberId(Long memberId);
 }
